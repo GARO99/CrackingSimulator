cc294d53cfdfc582cc61df19af0e0d55bf517ea1
Gustavo Andres Romero Ordoñez <gustavo.romero@bit-c.com.co>
2021-05-02 20:59:22 -0500

    Start project

---
+++ CrackingSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrackingSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
